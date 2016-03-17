4ba74e7ae37f8835de7317e1b12f48d9c16e4756
villion <wenic705@qq.com>
2016-03-17 16:26:10 +0800

    another one make a mistake,resolve it

--- myfirstGithub/src/net/villion/test/HelloWorld.java
+++ myfirstGithub/src/net/villion/test/HelloWorld.java
@@ -1,7 +1,7 @@
 package net.villion.test;
 
 public class HelloWorld {
-	public void sayHello(String name){
+	public static void sayHello(String name){
 		System.out.println("hello,"+name);
 	}
 	public static void main(String[] args){
